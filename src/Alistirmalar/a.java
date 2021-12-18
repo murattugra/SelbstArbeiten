785ba8637075a9ac8e075b8e8838dd9372f9ed24
Murat Arslan <mrterkmglsv@gmail.com>
2021-12-18 15:05:36 +0100

    ek

---
+++ src/Alistirmalar/a.java
@@ -0,0 +1,25 @@
+package Alistirmalar;
+
+public class a {
+
+	public static void main(String[] args) {
+		
+	
+		String phrase="a";
+		StringBuilder sb = new StringBuilder(phrase);      	
+		char ch='a';		
+		
+		char one = Character.toLowerCase(sb.charAt(0));      	
+		char two = Character.toLowerCase(ch);      	
+		
+		if (one == two) {
+		
+		System.out.println("true");
+		}
+		
+		
+		
+	}
+
+}
+	
---
+++ src/IntereiewSorular/InterviewQuestions_13.java
@@ -0,0 +1,16 @@
+package IntereiewSorular;
+
+public class InterviewQuestions_13 {
+
+	public static void main(String[] args) {
+		/*
+	     Write a Java Program to swap two numbers
+	         
+	     Swap (takas) islemini --> a=3, b=5 iken  a=5, b=3 degerlerini alması islemidir.
+	     iki yoldan yaptiginiz java kodunu yaziniz.
+	     1.Yol: 3. degisken kullanarak
+	     2.Yol: 3. degisken kullanmadan
+	*/
+	}
+
+}
