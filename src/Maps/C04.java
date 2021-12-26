284d0cbfaf780f09fb29ee5d40bd8325e23dc1c3
Murat Arslan <mrterkmglsv@gmail.com>
2021-12-26 10:41:05 +0100

    Map

--- src/Maps/C04.java
+++ src/Maps/C04.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class C04 {
 
@@ -72,8 +73,85 @@ public class C04 {
 		System.out.println(sayac+ ". entr : "+each.getKey()+each.getValue());
 		sayac++;
 	}
-	System.out.println(entry.containsAll(isimler));
+	
+	//Verilen bir String’deki harfleri ve harflerin
+	//kacar kez kullanildigini return eden bir method yaziniz
+	//   Ornek : Input : Hellooo  output : H=1, e=1, l=2, o=3
+	
+	
+	String input="Hellooo";
+	
+	String arr1[]=input.split("");//{H,e,l,l,o,o,o}
+	
+	Map<String,Integer> map1= new HashMap<>();
+	
+	for (String each : arr1) {
+		if (!(map1.containsKey(each))) {
+			map1.put(each, 1);
+		}else {
+			map1.put(each, map1.get(each)+1);
+		}
+	}
+	System.out.println(map1);
+	
+	
+	/*Soru 3 ) Verilen map’te istenen programlama dilini bilen kisileri list olarak 
+	 * donduren bir method yaziniz.
 		
+		map{ 101=Ali, Can, java, 102=Veli, Yan, java, 103=Ali, Yan, C#}
+		
+		Istenen dil  java
+		Sonuc  [Ali, Veli]		*/
+	
+	Map<Integer,String> map2=new TreeMap<>();
+	
+	map2.put(101, "Ali, Can, java");
+	map2.put(102, "Veli, Yan, java");
+	map2.put(103, "Ali, Yan, C#");
+	
+	
+	String istenilenDil="Java";
+	
+	List<String> lst2=new ArrayList<>(map2.values());
+	List<String> ayniBranslar=new ArrayList<>();
+	System.out.println(lst2);
+	String arr3[]=new String[3];
+
+	for (String each : lst2) {
+		
+		arr3=each.split(", ");
+		
+		if (arr3[2].equalsIgnoreCase(istenilenDil)) {
+			ayniBranslar.add(arr3[0]);
+		}
+	}
+	
+	
+	System.out.println(ayniBranslar);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
